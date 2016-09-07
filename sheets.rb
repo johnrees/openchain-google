f0b06afeb095ee310f5688c97a4b02d074d9c5aa
John Rees <john@bitsushi.com>
2016-09-07 10:20:57 +0100

    initial commit

---
+++ sheets.rb
@@ -0,0 +1,20 @@
+require "google_drive"
+
+# Creates a session. This will prompt the credential via command line for the
+# first time and save it to config.json file for later usages.
+session = GoogleDrive::Session.from_config("config.json")
+
+# Gets list of remote files.
+session.files.each do |file|
+  p file.title
+end
+
+# # Uploads a local file.
+# session.upload_from_file("/path/to/hello.txt", "hello.txt", convert: false)
+
+# # Downloads to a local file.
+# file = session.file_by_title("hello.txt")
+# file.download_to_file("/path/to/hello.txt")
+
+# # Updates content of the remote file.
+# file.update_from_file("/path/to/hello.txt")
